78f176368bd6688ba187e160d031de484308f74a
TetianaTantrum <tetiana.tantrum@gmail.com>
2023-02-25 00:24:08 +0200

    update

--- src/FriendList/FriendListItem.styled.jsx
+++ src/FriendList/FriendListItem.styled.jsx
@@ -1,6 +1,4 @@
 import styled from '@emotion/styled';
-// Status, FriendImg, Friendname;
-
 export const Status = styled.span`
   background-color: ${({ isOnline }) => (isOnline ? '#29bc27' : '#f72323')};
   border-radius: 50%;
